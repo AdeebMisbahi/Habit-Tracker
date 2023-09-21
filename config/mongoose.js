b4d9b1df031ed6bf4fe7be61b497a6e4f02a4e3a
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-09-21 08:56:47 +0530

    mongo db atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1/habit_tracker')
 
+const uri = "mongodb+srv://adeebmisbahi:test1234@cluster0.1ne6w3i.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect('mongodb://127.0.0.1/habit_tracker').then(()=>{
+mongoose.connect(uri).then(()=>{
     console.log('Connection successful!');
 }).catch((err) => console.log("no connection " + err));
 
